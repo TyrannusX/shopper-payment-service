029566c107d9b4fb215710f955a73a5db3c07126
Robert Reyes <neorob22@gmail.com>
2024-03-24 09:29:16 -0500

    updated database with environment variable

--- infrastructure/database.py
+++ infrastructure/database.py
@@ -1,7 +1,11 @@
 from pymongo import MongoClient
 from pymongo.collection import Collection
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+MONGODB_HOST_NAME = os.getenv("MONGODB_HOST_NAME")
 
-client = MongoClient("localhost", 27017)
+client = MongoClient(MONGODB_HOST_NAME, 27017)
 db = client.payment
 collection: Collection = db.payments
